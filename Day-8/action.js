7a7191369791a506d59e82d71b868f265e3ad125
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2024-01-18 19:13:30 +0530

    Day 8 Done

--- Day-8/action.js
+++ Day-8/action.js
@@ -1,3 +1,24 @@
+
+let count = 0
+let default_txt = "Count"
+let saved = ""
+
 function add(){
-    
+    count = count+1
+    document.getElementById("measure").innerText = count
+}
+
+function sub(){
+    count = count - 1
+    document.getElementById("measure").innerText = count
+}
+
+function reset(){
+    count = 0
+    document.getElementById("measure").innerText = count
+}
+
+function save(){
+    saved = saved + " "+ count
+    document.getElementById("space").innerText = saved
 }
