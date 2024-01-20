f0b0deba577233bdfd9a1c331b133d189b5834d2
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2024-01-20 22:26:53 +0530

    commit 1

---
+++ Day-7/action.js
@@ -0,0 +1,13 @@
+let form_text = document.createElement("input")
+let add_count = 1
+let sub_count = 0 
+let form = "form"
+let array = ["name","email"]
+
+function add(){
+    
+}
+
+function rm(){
+
+}
