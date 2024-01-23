504514acb776fc1780107fc3bd4b4541db9f278f
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2024-01-23 22:07:54 +0530

    Day 5 done

--- Day-5/action.js
+++ Day-5/action.js
@@ -1,7 +1,23 @@
+let password =""
 let words = ["enemy","military","idea","plan","incompetant","party","demon","guilt","deadly","uniform","enormous","founder","bait",
-            "martyrs","raid",""
+            "martyrs","raid","jaw","armour","angels","enemy","war","giant","hurry"
             ]
+let special_chars = [0,1,2,3,4,5,6,7,8,9,"!","@","#","$","%","&","*"]
+
+function rand_el(arr){
+    index = Math.floor(Math.random()*arr.length)
+    return arr[index]
+}
 
 function generate(){
-  
+  password = password + rand_el(words) + rand_el(special_chars) + rand_el(words) + rand_el(special_chars) + rand_el(words)
+  document.getElementById("text").innerHTML = password
+  password = ""
+}
+
+function copy(){
+    txt = document.getElementById("text")
+    txt.select()
+    txt.setSelectionRange(0,99999)
+    navigator.clipboard.writeText(txt.innerText).then(() => alert('Text copied'));
 }
