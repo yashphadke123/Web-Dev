3b6de7670cff811e45ccc1748a632d9172c90558
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2024-01-27 19:25:59 +0530

    Day 4 finally done

--- Day-4 Digital Clock/js/action.js
+++ Day-4 Digital Clock/js/action.js
@@ -1,6 +1,17 @@
-let h = 11;
-let m = 34;
-let s = 54;
+function time(){
+    let current_time = new Date()
+    h = current_time.getHours()
+    s = current_time.getSeconds()
+    m = current_time.getMinutes()
+    document.getElementById("hours").innerHTML = h
+    document.getElementById("minutes").innerHTML = m
+    document.getElementById("seconds").innerHTML = s
+    if(h>12){
+        document.getElementById("time").innerHTML = "PM"
+    }
+    else{
+        document.getElementById("time").innerHTML = "AM"
+    }
+}
 
-let hours = document.getElementById("hours")
-hours.innerText = h;
+setInterval(time,1000)
