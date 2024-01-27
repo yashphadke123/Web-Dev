56ef465724dcb61f30d16117f0e91c5876ede71a
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2024-01-27 17:57:04 +0530

    shit ass day

--- Day-27 Memory Game/action.js
+++ Day-27 Memory Game/action.js
@@ -1,1 +1,12 @@
-function for_push(){}
+function apple1(){
+    document.getElementById("apple1").style.backgroundColor = "red"
+}
+function apple2(){
+    document.getElementById("apple2").style.backgroundColor = "red"
+}
+function banana1(){
+    document.getElementById("banana1").style.backgroundColor = "yellow"
+}
+function banana2(){
+    document.getElementById("banana2").style.backgroundColor = "yellow"
+}
