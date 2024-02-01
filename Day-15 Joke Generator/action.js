1d7d4f8a804ae67464790c2e02a6aa9664688df3
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2024-02-01 18:46:23 +0530

    day 15 done

--- Day-15 Joke Generator/action.js
+++ Day-15 Joke Generator/action.js
@@ -1,1 +1,16 @@
 // api key 0hhDopMc9mygfv3o063kzA==oL5DXoQWlwv20Fu9
+let options = {
+    method : 'GET',
+    headers: {'x-api-key':"0hhDopMc9mygfv3o063kzA==oL5DXoQWlwv20Fu9"}
+}
+let url = "https://api.api-ninjas.com/v1/jokes?limit=1"
+
+function temp(data){
+    document.getElementById("text").innerHTML = data[0].joke
+}
+
+function joke_gen(){
+    fetch(url,options)
+        .then(res => res.json())
+        .then(data => temp(data))
+}
